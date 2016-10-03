4e4f6d56ce3fe785161dc0b4a9660faf5bb1dddc
Lucas <lvs.viveiros@gmail.com>
2016-10-03 17:45:52 -0300

    TAXA JUROS - VALIDAÇÃO DE CAMPOS

--- app/src/main/java/br/com/lampmobile/flagment/MesesFragment.java
+++ app/src/main/java/br/com/lampmobile/flagment/MesesFragment.java
@@ -7,17 +7,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 import br.com.lampmobile.R;
+import br.com.lampmobile.utils.Utils;
 
 
-public class MesesFragment extends Fragment {
+public class MesesFragment extends Fragment implements View.OnClickListener{
+
+    EditText valorParcelas;
+    EditText taxaJuros;
+    EditText valorFinanciado;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_meses, container, false);
+
+        valorParcelas = (EditText) rootView.findViewById(R.id.mesesValorParcelas);
+        taxaJuros = (EditText) rootView.findViewById(R.id.mesesJuros);
+        valorFinanciado = (EditText) rootView.findViewById(R.id.mesesValorFinanciamento);
+
+        Button calcular = (Button) rootView.findViewById(R.id.mesesCalcular);
+        calcular.setOnClickListener(this);
 
         // INICIALIZA PROPAGANDA
         AdView mAdView = (AdView) rootView.findViewById(R.id.mesesAdView);
@@ -25,5 +41,27 @@ public class MesesFragment extends Fragment {
         mAdView.loadAd(adRequest);
 
         return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //Fechar teclado
+        Utils.fecharTeclado(super.getActivity());
+
+        if(taxaJuros.getText() == null || taxaJuros.getText().toString().isEmpty()){
+            Toast.makeText(super.getContext(), "Favor informar Taxa de Juros!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(valorParcelas.getText() == null || valorParcelas.getText().toString().isEmpty()){
+            Toast.makeText(super.getContext(), "Favor informar Valor Parcelas!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(valorFinanciado.getText() == null || valorFinanciado.getText().toString().isEmpty()){
+            Toast.makeText(super.getContext(), "Favor informar Valor Financiado!", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
--- app/src/main/java/br/com/lampmobile/flagment/TaxaJurosFragment.java
+++ app/src/main/java/br/com/lampmobile/flagment/TaxaJurosFragment.java
@@ -5,16 +5,32 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 
 import br.com.lampmobile.R;
+import br.com.lampmobile.utils.Utils;
 
-public class TaxaJurosFragment extends Fragment {
+public class TaxaJurosFragment extends Fragment implements View.OnClickListener {
+
+    EditText numeroMeses;
+    EditText valorParcelas;
+    EditText valorFinanciado;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_taxa_juros, container, false);
+
+        numeroMeses = (EditText) rootView.findViewById(R.id.txJurosNumeroMeses);
+        valorParcelas = (EditText) rootView.findViewById(R.id.txJurosParcelas);
+        valorFinanciado = (EditText) rootView.findViewById(R.id.txJurosValorFinanciamento);
+
+        Button calcular = (Button) rootView.findViewById(R.id.txJurosCalcular);
+        calcular.setOnClickListener(this);
 
         // INICIALIZA PROPAGANDA
         AdView mAdView = (AdView) rootView.findViewById(R.id.txJurosAdView);
@@ -22,5 +38,25 @@ public class TaxaJurosFragment extends Fragment {
         mAdView.loadAd(adRequest);
 
         return rootView;
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        //Fechar teclado
+        Utils.fecharTeclado(super.getActivity());
+
+        if(valorFinanciado.getText() == null || valorFinanciado.getText().toString().isEmpty()){
+            Toast.makeText(super.getContext(), "Favor informar Valor Financiado!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(valorParcelas.getText() == null || valorParcelas.getText().toString().isEmpty()){
+            Toast.makeText(super.getContext(), "Favor informar Valor das Parcelas!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(numeroMeses.getText() == null || numeroMeses.getText().toString().isEmpty()){
+            Toast.makeText(super.getContext(), "Favor informar Quantidade de Meses!", Toast.LENGTH_SHORT).show();
+            return;
+        }
     }
 }
